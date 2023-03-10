ae4370d8c8da0cf28d5b0de1c9121bd2bb952085
Benjamin Kechmiri <benjamin.kechmiri@learnet.se>
2023-03-10 13:32:17 +0100

    added_links

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -4,6 +4,7 @@ import TVW from '../assets/TVW.png'
 
 export default [
     {
+        link: "https://docs.google.com/document/d/10ZhEESBaks8fnwgCarf2OIjkvQOMJ3vBp3jjXZJ7K_o/edit?usp=sharing",
         title: 'Social Media & Mental Health',
         imgUrl: bob,
         stack: ['Sociology','Psychology','Swedish']
@@ -16,6 +17,7 @@ export default [
         link: "https://github.com/benjibooy/Chess.git"
     },
     {
+        link: "https://github.com/benjibooy/Portfolio.git",
         title: 'This Very Website',
         imgUrl: TVW,
         stack: ['React','Tailwind','Vite']
